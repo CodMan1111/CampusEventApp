408afe6fe9702140c7f4b1f1ab19c85be3654cfc
Lucas Deutsch <lucasdeutsch@Lucass-MacBook-Pro.local>
2026-04-15 13:17:48 -0400

    welcome start commit

--- app/src/main/java/com/example/campuseventapp/MainActivity.kt
+++ app/src/main/java/com/example/campuseventapp/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.campuseventapp.ui.theme.CampusEventAppTheme
+import com.example.campuseventapp.WelcomeScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,28 +21,14 @@ class MainActivity : ComponentActivity() {
         setContent {
             CampusEventAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Text(text = "Campus Event Notifier", Modifier.padding(innerPadding))
                 }
             }
         }
-    }
-}
+        @Composable
+        fun Welcome(modifier: Modifier) {
+            Text(text = "Campus Event Notifier", modifier.padding())
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CampusEventAppTheme {
-        Greeting("Android")
+        }
     }
 }
---
+++ app/src/main/java/com/example/campuseventapp/WelcomeScreen.kt
@@ -0,0 +1,12 @@
+package com.example.campuseventapp
+
+import androidx.compose.runtime.Composable
+import androidx.compose.material3.Text
+class WelcomeScreen {
+
+    }
+@Composable
+fun Welcome() {
+    Text(text = "Campus Event Notifier")
+
+}
